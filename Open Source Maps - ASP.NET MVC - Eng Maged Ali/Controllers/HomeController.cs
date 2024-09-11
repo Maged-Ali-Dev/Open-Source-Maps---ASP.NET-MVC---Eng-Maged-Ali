bcaae07b3a15c4c10d0dbae377e7d92e4cab9f55
Maged Ali <Maged.ali89@hotmail.com>
2024-09-11 20:08:25 +0300

    Add project files.

---
+++ Open Source Maps - ASP.NET MVC - Eng Maged Ali/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Open_Source_Maps___ASP.NET_MVC___Eng_Maged_Ali.Models;
+using System.Diagnostics;
+
+namespace Open_Source_Maps___ASP.NET_MVC___Eng_Maged_Ali.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Open Source Maps - ASP.NET MVC - Eng Maged Ali/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Open_Source_Maps___ASP.NET_MVC___Eng_Maged_Ali.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
